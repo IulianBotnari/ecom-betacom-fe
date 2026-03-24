dc26a80a637b7894be2479c835a39c52e6ec9602
IulianBotnari <iulianbotnari93@gmail.com>
2026-03-24 17:46:10 +0100

    first structure

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,19 +3,19 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Homepage } from './component/homepage/homepage';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { About } from './pages/about/about';
+import { Login } from './pages/login/login';
+import { Signin } from './pages/signin/signin';
+import { Products } from './pages/products/products';
+import { UserDetails } from './pages/user-details/user-details';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideClientHydration(withEventReplay()),
-  ],
-  bootstrap: [App]
+  declarations: [App, Homepage, Header, Footer, About, Login, Signin, Products, UserDetails],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}

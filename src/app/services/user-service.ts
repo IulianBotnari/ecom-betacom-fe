873a60fecca463ca76a1bb596f0e93244fd8fa05
meneguzzoS <meneguzzo_stefano@hotmail.it>
2026-03-25 17:05:46 +0100

    creazione service per user

---
+++ src/app/services/user-service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+    private url = "http://localhost:9090/rest/user/";
+
+    constructor(private http: HttpClient) {}
+
+    create (body: {}) {
+      return this.http.post(this.url + "create", body);
+    }
+
+    update (body: {}) {
+      return this.http.put(this.url + "update", body);
+    }
+
+    listAll () {
+      return this.http.get(this.url + "listAll");
+    }
+
+    getById (id: number) {
+      return this.http.get(this.url + "findById/" + id);
+    }
+    
+    delete (id: number) {
+      return this.http.get(this.url + "delete/" + id);
+    }
+}

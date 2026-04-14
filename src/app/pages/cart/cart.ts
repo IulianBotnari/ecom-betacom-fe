c6e22d34615ccadee33b021ea7a08c7491d2d339
IulianBotnari <iulianbotnari93@gmail.com>
2026-04-14 09:32:49 +0200

    fix cart

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal,effect } from '@angular/core';
+import { Component, computed, inject, OnInit, signal, effect } from '@angular/core';
 import { CartService } from '../../services/cart-services';
 import { CartItemsService } from '../../services/cart-items-service';
 import { OrderService } from '../../services/order-service';
@@ -6,7 +6,6 @@ import { UserService } from '../../services/user-service';
 import { forkJoin, switchMap } from 'rxjs';
 import { OrderItemServices } from '../../services/order-item-services';
 import { error, log } from 'console';
-
 
 @Component({
   selector: 'app-cart',
@@ -16,138 +15,139 @@ import { error, log } from 'console';
 })
 export class Cart implements OnInit {
   private cartService = inject(CartService);
-  private cartitemservices = inject(CartItemsService)
-  private orderServices = inject(OrderService)
-  private userServices = inject (UserService)
-  private orderItemServices = inject(OrderItemServices)
+  private cartitemservices = inject(CartItemsService);
+  private orderServices = inject(OrderService);
+  private userServices = inject(UserService);
+  private orderItemServices = inject(OrderItemServices);
 
-  cart = signal <any|null>(null);
-  isInitialLoad: boolean = true; 
-  
+  cart = signal<any | null>(null);
+  isInitialLoad: boolean = true;
+
   cartTotal = computed(() => {
-  return this.cart().cartItems.reduce((acc:any, item:any) => {
-    return acc + (item.product?.price || 0) * item.quantity;
-  }, 0);
-});
+    const currentCart = this.cart();
+    if (!currentCart || !currentCart.cartItems) return 0;
 
-  userId = Number( localStorage.getItem('userId'))
-  user = signal <any|null>(null); 
+    return currentCart.cartItems.reduce((acc: any, item: any) => {
+      return acc + (item.product?.price || 0) * item.quantity;
+    }, 0);
+  });
+
+  userId = Number(localStorage.getItem('userId'));
+  user = signal<any | null>(null);
   addressUser = computed(() => {
-  const user = this.user();
-  
-  if (user && user.addresses) {
-    
-    return user.addresses.find((addr: any) => addr.defaultAddress === true);
-  }
-  
-  return null; 
-});
+    const user = this.user();
+    if (user && user.addresses) {
+      return user.addresses.find(
+        (addr: any) =>
+          addr.defaultAddress === true ||
+          addr.defaultAddress === 'true' ||
+          addr.defaultAddress === 1,
+      );
+    }
+    return null;
+  });
 
   ngOnInit(): void {
     this.loadCart(this.userId);
-    this.loadUser(Number( localStorage.getItem('userId')))
-    
+    this.loadUser(Number(localStorage.getItem('userId')));
   }
 
-  loadUser(id:number){
+  loadUser(id: number) {
     this.userServices.getById(id).subscribe({
-      next:(res:any) => {
-        this.user.set(res)
-      }, 
-      error: (err:any) =>{
-        console.log(err)
-      }
-    }) 
-  }
- 
-  
-
-  loadCart(userId:number){
-    this.cartService.getByUSerId(userId).subscribe({
-      next:(res:any) => {
-        this.cart.set(res)
-      }, 
-      error:(err:any) =>{
+      next: (res: any) => {
+        this.user.set(res);
+      },
+      error: (err: any) => {
         console.log(err);
-        
-      }
-    })
-
+      },
+    });
   }
- 
 
-  updateQuantity(itemId: number, delta: number, actualqty:number) {
-  const newQuantity = actualqty + delta;
-  if (newQuantity <= 0) return;
+  loadCart(userId: number) {
+    this.cartService.getByUSerId(userId).subscribe({
+      next: (res: any) => {
+        this.cart.set(res);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
 
-  const request = {
-    cartItemId: itemId,
-    quantity: newQuantity,
-    cartId: this.cart().id
-    
-  };
+  updateQuantity(itemId: number, delta: number, actualqty: number) {
+    const newQuantity = actualqty + delta;
+    if (newQuantity <= 0) return;
 
-  this.cartitemservices.update(request).subscribe({
-    next: (res:any) => {
-     // this.cart.set(res)
-     console.log('risposta update CartItem: +',res)
-      this.loadCart(this.userId)
-    },
-    error: (err) => {
-    console.log(err)
-      
-    }
-  });
+    const request = {
+      cartItemId: itemId,
+      quantity: newQuantity,
+      cartId: this.cart().id,
+    };
 
-}
+    this.cartitemservices.update(request).subscribe({
+      next: (res: any) => {
+        // this.cart.set(res)
+        console.log('risposta update CartItem: +', res);
+        this.loadCart(this.userId);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 
-buy() {
-  console.log(this.addressUser());
-  
-  const orderBody = {
-    userId: this.userId, 
-    shippingAddress: this.addressUser().id,
-    status: "ORDINATO"
-  };
+  buy() {
+    const currentAddress = this.addressUser();
+    const currentCart = this.cart();
+    console.log('Indirizzo:', currentAddress);
+    console.log('Carrello:', currentCart);
 
-  this.orderServices.create(orderBody).pipe(
-    
-    switchMap((res) => {
-      this.loadCart(this.userId);
-      return this.orderServices.getByUserId(this.userId);
-    }),
+    console.log('UserId:', this.userId);
 
-    
-    switchMap((resOrder:any) => {
-      
-      const itemsRequests = this.cart().cartItems.map((item:any) => {
-        const orderItemBody = {
-          orderId: resOrder.id,
+    const itemsToProcess = [...this.cart().cartItems];
+
+    if (!currentAddress || !currentCart) return;
+
+    const orderBody = {
+      userId: this.userId,
+      shippingAddress: currentAddress.id,
+      paymentMethodId: 1,
+      status: 'ORDINATO',
+    };
+
+    this.orderServices.create(orderBody).pipe(
+        switchMap((newOrder: any) => {
+         if (!newOrder || !newOrder.id) {
+        throw new Error("Il server non ha restituito l'ID dell'ordine");
+      }
+
+      console.log('Ordine creato, ID:', newOrder.id);
+
+      const itemsRequests = itemsToProcess.map((item: any) => {
+        const detailPayload = {
+          orderId: newOrder.id, 
           productId: item.product.id,
           sizeId: item.size.id,
           quantity: item.quantity,
           totalPrice: item.quantity * (item.product.discount ?? item.product.price)
         };
-        return this.orderItemServices.create(orderItemBody);
+        
+        console.log('Invio dettaglio:', detailPayload);
+        return this.orderItemServices.create(detailPayload);
       });
 
-      
       return forkJoin(itemsRequests);
     })
   ).subscribe({
     next: (results) => {
-      console.log('Tutti i prodotti sono stati salvati:', results);
-      
+      console.log('Tutto salvato con successo:', results);
+      this.loadCart(this.userId);
+      alert('Ordine effettuato con successo!');
     },
-    error: (err) => console.error('Errore durante il checkout:', err)
+    error: (err) => {
+      console.error('Errore nel checkout:', err);
+  
+    }
   });
 }
-
-   
 }
-
-
-
-  
-
-
